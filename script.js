deb82235c2c6469e3ae98ec17f1db1540a3ca9f2
mfssouza <mayke@email>
2025-11-18 09:57:35 -0300

    Adição do botão de compra do produto.

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ btTrailer = document.querySelector("#Trailer1")
 btTrailer.addEventListener ("click", ()=>
 {
     this.document.location = "https://www.youtube.com/watch?v=PyMlV5_HRWk"
-}) 
+})
+
+btAdquirir = document.querySelector("#Adquirir")
+
+btAdquirir.addEventListener ("click", ()=>
+{
+    this.document.location = "https://www.xbox.com/pt-br/games/store/halo-infinite-campaign/9np1p1wfs0lb"
+})
